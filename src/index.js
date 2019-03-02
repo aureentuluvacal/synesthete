12a96ddb3cd3e0206503c36030c03aefb398dde1
Caryssa Perez <caryssaperez@gmail.com>
2019-03-02 13:51:39 -0500

    Configure react

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function bigComputation() {
+  alert('Big computation in JavaScript');
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Synesthete</h1>
+      <button onClick={bigComputation}>Run Computation</button>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
